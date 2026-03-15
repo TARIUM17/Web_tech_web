7d76e95b7266e2fe95fbd4bff1c541599699f827
TARIUM17 <u384388@gmail.com>
2026-03-15 12:40:21 +0300

    Created profile paige and connect database

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,26 +1,26 @@
-        const video = document.getElementById('bg-video');
-        const resumeBtn = document.getElementById('resume-btn');
+        // const video = document.getElementById('bg-video');
+        // const resumeBtn = document.getElementById('resume-btn');
         
-        const STOP_TIME = 5; 
-        let is_Paused = false;
+        // const STOP_TIME = 5; 
+        // let is_Paused = false;
 
-        video.addEventListener('loadedmetadata', () => {
-            video.playbackRate = 4; 
-        });
+        // video.addEventListener('loadedmetadata', () => {
+        //     video.playbackRate = 4; 
+        // });
 
-        video.addEventListener('timeupdate', () => {
-            if (!is_Paused && video.currentTime >= STOP_TIME) {
-                    video.pause();
-                    is_Paused = true;
-                    resumeBtn.style.display = 'inline-block';
-            }
-        });
+        // video.addEventListener('timeupdate', () => {
+        //     if (!is_Paused && video.currentTime >= STOP_TIME) {
+        //             video.pause();
+        //             is_Paused = true;
+        //             resumeBtn.style.display = 'inline-block';
+        //     }
+        // });
 
-        resumeBtn.addEventListener('click', () => {
-            video.play();
-            resumeBtn.style.display = 'none';
-        });
+        // resumeBtn.addEventListener('click', () => {
+        //     video.play();
+        //     resumeBtn.style.display = 'none';
+        // });
         
-        video.addEventListener('play', () => {
-            isPaused = true;
-        });
+        // video.addEventListener('play', () => {
+        //     isPaused = true;
+        // });
